fd5034d11c878830193ad4e1eedaec9ee615e6a5
Jennifer <142938327+Jennifercruz23@users.noreply.github.com>
2025-08-24 12:24:07 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/speechrecognition/main.go
@@ -0,0 +1,23 @@
+// cmd/speechrecognition/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "speechrecognition/internal/speechrecognition"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := speechrecognition.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
